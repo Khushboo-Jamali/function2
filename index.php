0ba5fff72ee7af852c563d46f6fec1719e8847a7
khushboo-jamali <jamalikhushboo@gmail.com>
2024-07-11 12:28:16 +0100

    php functions

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // function num($start)
+    // {
+
+    //     if ($start <= 10) {
+    //         echo $start . "<br>";
+
+    //         $start++;
+    //         num($start);
+    //     } else {
+    //         return;
+    //     }
+    // }
+    // num(0);
+    // $arr = ["hello", "world", "how", "are", "you"];
+    // echo "<pre>";
+    // print_r($arr);
+    // echo "</pre>";
+
+    // $str = implode("", $arr);
+    // echo $str;
+
+
+    // $str = "hello world how are you ";
+    // echo   str_word_count($str) . "<br>";
+    // echo   strlen($str) . "<br>";
+    // echo   str_repeat("hello", 2) . "<br>";
+    // $num = 5 / 2;
+    // echo ceil($num) . "<br>";
+    // echo floor($num) . "<br>";
+
+    // echo pow(2, 3);
+    // echo sqrt(64);
+    // echo date_default_timezone_set("PAKISTAN");
+    //  echo "<pre>";
+    echo "<pre> <br>";
+
+
+    print_r(getdate())
+
+
+    // $arr = explode('  ', $str, 2);
+
+    // echo "<pre> <br>";
+    // print_r($arr); 
+    // echo "</pre>";
+
+
+    ?>
+</body>
+
+</html>
